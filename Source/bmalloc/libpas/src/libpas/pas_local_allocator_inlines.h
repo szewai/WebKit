39a5ac27139893e6369b2b491c0ec29e816eac5d
Dan Hecht <dan.hecht@apple.com>
2025-12-12 23:03:48 -0800

    [libpas] fix lock-free reads of the medium directory tuple table https://bugs.webkit.org/show_bug.cgi?id=301940 rdar://163964480
    Reviewed by Yusuke Suzuki and Keith Miller.
    
    Some paths read the medium directory tuple table using a lock-free
    algorithm. However, they return a pointer into this table and
    load from the pointer after checking for mutations to the table,
    potentially reading the wrong entry.
    
    Fix this by performing the read in the lock free critical section
    (unless the heap lock is held along the path, in which case the pointer
    is still used).
    
    Thanks to Phil Pizlo for finding the issue and proposing the fix.
    
    Testing: added asserts to verify this race condition
    
    * Source/bmalloc/libpas/src/libpas/pas_local_allocator_inlines.h:
    (pas_local_allocator_try_allocate):
    * Source/bmalloc/libpas/src/libpas/pas_segregated_heap.c:
    (medium_directory_tuple_for_index_impl):
    (medium_directory_tuple_for_index_with_lock):
    (pas_segregated_heap_medium_directory_tuple_for_index):
    (pas_segregated_heap_medium_allocator_index_for_index):
    (pas_segregated_heap_medium_size_directory_for_index):
    (pas_segregated_heap_ensure_allocator_index):
    (pas_segregated_heap_ensure_size_directory_for_size):
    * Source/bmalloc/libpas/src/libpas/pas_segregated_heap.h:
    (pas_segregated_heap_medium_directory_result_create_empty):
    
    Originally-landed-as: 301765.261@safari-7623-branch (1164239a5354). rdar://166336234
    Canonical link: https://commits.webkit.org/304406@main

--- Source/bmalloc/libpas/src/libpas/pas_local_allocator_inlines.h
+++ Source/bmalloc/libpas/src/libpas/pas_local_allocator_inlines.h
@@ -1833,6 +1833,7 @@ pas_local_allocator_try_allocate(pas_local_allocator* allocator,
     pas_allocation_result result;
 
     PAS_TESTING_ASSERT(!allocator->scavenger_data.is_in_use);
+    PAS_TESTING_ASSERT(size <= allocator->object_size || !allocator->object_size);
 
     if (verbose) {
         pas_log("Allocator %p (%s) allocating size = %zu, alignment = %zu.\n",
@@ -1854,6 +1855,7 @@ pas_local_allocator_try_allocate(pas_local_allocator* allocator,
                     (void*)result.begin);
         }
     
+        PAS_TESTING_ASSERT(size <= allocator->object_size);
         return result_filter(
             pas_allocation_result_create_success_with_zero_mode(result.begin, result.zero_mode));
     }
@@ -1875,6 +1877,7 @@ pas_local_allocator_try_allocate(pas_local_allocator* allocator,
             allocator, allocation_mode, counts, result_filter);
         if (verbose)
             pas_log("in small segregated slow return - result.begin = %p\n", (void*)result.begin);
+        PAS_TESTING_ASSERT(size <= allocator->object_size);
         return result;
     }
 
@@ -1882,6 +1885,7 @@ pas_local_allocator_try_allocate(pas_local_allocator* allocator,
         allocator, size, alignment, allocation_mode, counts, result_filter);
     if (verbose)
         pas_log("in generic return - result.begin = %p\n", (void*)result.begin);
+    PAS_TESTING_ASSERT(size <= allocator->object_size);
     return result;
 }
 
