30d7ee170e0d91d896e58d192463f9737b41924d
Robert Jenner <jenner@apple.com>
2025-12-18 14:13:27 -0800

    Unreviewed, reverting 304680@main (ffdd8656aebc) rdar://166820676
    Broke builds.
    
    Reverted change:
    
        Resync libvpx up to M143
        rdar://161667351
        https://bugs.webkit.org/show_bug.cgi?id=304397
        304680@main (ffdd8656aebc)
    
    Canonical link: https://commits.webkit.org/304705@main

--- Source/ThirdParty/libwebrtc/Source/third_party/libvpx/source/libvpx/vp9/encoder/vp9_ratectrl.c
+++ Source/ThirdParty/libwebrtc/Source/third_party/libvpx/source/libvpx/vp9/encoder/vp9_ratectrl.c
@@ -856,19 +856,15 @@ int vp9_rc_regulate_q(const VP9_COMP *cpi, int target_bits_per_frame,
           frame_type, i, correction_factor, cm->bit_depth);
     }
 
-    int diff_bits = (int)VPXMIN(
-        VPXMAX(((int64_t)target_bits_per_mb - (int64_t)bits_per_mb_at_this_q),
-               -INT_MAX),
-        INT_MAX);
     if (bits_per_mb_at_this_q <= target_bits_per_mb) {
-      if (diff_bits <= last_error)
+      if ((target_bits_per_mb - bits_per_mb_at_this_q) <= last_error)
         q = i;
       else
         q = i - 1;
 
       break;
     } else {
-      last_error = -diff_bits;
+      last_error = bits_per_mb_at_this_q - target_bits_per_mb;
     }
   } while (++i <= active_worst_quality);
 
