56033f477def5b1263edfa459347718ab0184c37
Adrian Perez de Castro <aperez@igalia.com>
2025-12-20 02:47:28 -0800

    [GLib] Drop WTF_ALLOW_UNSAFE_BUFFER_USAGE in WebKitURIRequest.cpp https://bugs.webkit.org/show_bug.cgi?id=304526
    Reviewed by Carlos Garcia Campos.
    
    * Source/WebKit/Shared/API/glib/WebKitURIRequest.cpp:
    (webkit_uri_request_set_uri): Drop unneeded unsafe buffer suppresion,
    the acessed sObjProperties variable is already an std::array and does
    not trigger the compiler check.
    
    Canonical link: https://commits.webkit.org/304816@main

--- Source/WebKit/Shared/API/glib/WebKitURIRequest.cpp
+++ Source/WebKit/Shared/API/glib/WebKitURIRequest.cpp
@@ -151,9 +151,7 @@ void webkit_uri_request_set_uri(WebKitURIRequest* request, const char* uri)
 
     request->priv->resourceRequest.setURL(WTF::move(url));
 
-    WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN // GTK/WPE port
     g_object_notify_by_pspec(G_OBJECT(request), sObjProperties[PROP_URI]);
-    WTF_ALLOW_UNSAFE_BUFFER_USAGE_END
 }
 
 /**
