szewai/WebKit

commit 6511e21704cb8062310091bf8a80729dd38e9981

[Tools][WPE][GTK][SDK] Build webkit in a common directory at /sdk/webkit when using the SDK https://bugs.webkit.org/show_bug.cgi?id=303645
Reviewed by Nikolas Zimmermann.

Since we moved out of flatpak SDK to the new Container SDK we miss the feature
that it had Flatpak of building webkit in /app/webkit, as that allowed reusing
remote caches (ccache, sccache) when developers have the webkit checkout in
different directories very easy.

For ccache I tried a workaround at 301772@main but that ended causing issues
with GDB and the paths to the source files. Also for sccache that workaround
is not possible, sccache does not support at all sharing caches if the build
path on disk changes.

So this patch introduces a wrapper that can create a symlink from /sdk/webkit
to the webkit source directory and that also can create a private mount
namespace where /sdk/webkit is bind-mounted to the WebKit source directory.

That private mount namespace is only visible to the pid group executed by the
wrapper, so there can be multiple webkit builds or tests running each one
with its own private view of the /sdk/webkit bind-mount without interfering
between them. This allow to build or test from different webkit checkout paths
and each one of those processes would have a different /sdk/webkit mount point.

The symlink is created to support any test runner or program that doesn't run
with this wrapper, for example when using GDB to debug, that way the paths inside
/sdk/webkit can be resolved automatically.

For the symlink is not possible to have different views, so in case of working
from two different checkouts the last directory from where webkit was built, or
from where the tests were executed would be the one resolved.

This is not perfect, but I think is good enough. For example, if you run GDB it
is likely that before invoking GDB you would have executed any of the scripts
that use this wrapper (build-webkit, run-webkit-tests, run-minibrowser, etc) so

Tools/Scripts/test262/Runner.pm
@@ -323,6 +323,7 @@ sub processCLI {
 sub setupEnvironment()
 {
     if ($^O eq "linux") {
+        maybeUseContainerSDKRootDir();
         setupUnixWebKitEnvironment(productDir());
     }
 }

Source/cmake/WebKitCompilerFlags.cmake
@@ -142,13 +142,6 @@ if (DEVELOPER_MODE OR ARM)
     WEBKIT_PREPEND_GLOBAL_COMPILER_FLAGS(-fno-omit-frame-pointer)
 endif ()
 
-# Record references to files using relative paths instead of absolute.
-# This helps both with reproducible builds and ccache hits.
-# It also breaks debugedit, so limit this to DEVELOPER_MODE.
-if (DEVELOPER_MODE)
-    WEBKIT_PREPEND_GLOBAL_COMPILER_FLAGS(-ffile-prefix-map=${CMAKE_SOURCE_DIR}=.)
-endif ()
-
 if (COMPILER_IS_GCC_OR_CLANG)
     if (COMPILER_IS_CLANG OR (DEVELOPER_MODE AND NOT ARM))
         # Split debug information in ".debug_types" / ".debug_info" sections - this leads

Tools/Scripts/browserperfdash-benchmark
@@ -27,6 +27,8 @@ import logging
 import sys
 
 from webkitpy.browserperfdash.browserperfdash_runner import main, format_logger
+from webkitpy.port.linux_container_sdk_utils import maybe_use_container_sdk_root_dir
+maybe_use_container_sdk_root_dir()
 
 _log = logging.getLogger()
 

Tools/Scripts/build-webkit
@@ -60,6 +60,7 @@ if (shouldBuildForCrossTarget()) {
     my @command = (File::Spec->catfile(sourceDir(), "Tools", "Scripts", "build-webkit"));
     runInFlatpak(@command);
 }
+maybeUseContainerSDKRootDir();
 
 my $originalWorkingDirectory = getcwd();
 chdirWebKit();

Tools/Scripts/container-sdk-rootdir-wrapper
@@ -1,0 +1,186 @@
+#!/usr/bin/env sh
+#
+# Copyright (C) 2025 Igalia S.L.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+set -eu
+
+SCRIPTPATH="$(realpath -- "${0}")"
+SCRIPTDIR="$(dirname -- "${SCRIPTPATH}")"
+WEBKITDIR="$(realpath -- "${SCRIPTDIR}/../..")"
+WEBKIT_SOURCE_DIR="${WEBKIT_SOURCE_DIR:-${WEBKITDIR}}"
+WEBKIT_SOURCE_DIR="$(realpath -m -- "${WEBKIT_SOURCE_DIR}")"
+
+help() {
+    cat <<EOF
+Usage: ${0} program-to-execute [arguments...]
+       ${0} --create-symlink
+
+This script is used by the WebKit tooling when running inside the
+webkit-container-sdk for GTK/WPE port development. It enables building
+WebKit and running tests inside a common path at /sdk/webkit.
+
+Options:
+    -h, --help         Show this help message
+    --create-symlink   Create /sdk/webkit as a symlink to ${WEBKIT_SOURCE_DIR}
+                       and exit. Useful for tools not executed through this
+                       wrapper (like gdb) that need to resolve /sdk/webkit paths.
+
+Default behavior:
+    Creates a private mount namespace where /sdk/webkit is a bind-mount
+    to ${WEBKIT_SOURCE_DIR}, then executes the specified program.
+
+Examples:
+    ${0} Tools/Scripts/build-webkit --wpe --release
+    ${0} --create-symlink
+EOF
+    exit 0
+}
+
+error() {
+    printf '%s\n' "${@}" 1>&2
+    exit 1
+}
+
+# If path starts with ${WEBKIT_SOURCE_DIR}, replace that prefix with /sdk/webkit.
+maybe_replace_path_prefix_to_rootsdk() {
+    case "${1}" in
+        "${WEBKIT_SOURCE_DIR}"|"${WEBKIT_SOURCE_DIR}"/*)
+            printf '%s\n' "/sdk/webkit${1#"${WEBKIT_SOURCE_DIR}"}"
+        ;;
+        *)
+            printf '%s\n' "${1}"
+        ;;
+    esac
+}
+
+resolve_relpath_to_abs_if_outside() {
+    abs=$(realpath -m -- "${ORGPWD}/${1}")
+    case "${abs}" in
+        "${WEBKIT_SOURCE_DIR}"|"${WEBKIT_SOURCE_DIR}"/*)
+            # Inside the tree: keep original relative value
+            printf '%s\n' "${1}"
+        ;;
+        *) # relative path outside the tree: make it absolute
+            printf '%s\n' "${abs}"
+        ;;
+    esac
+}
+
+maybe_update_relpath_to_abs_if_outside() {
+    case "${1}" in
+        ""|/*|*://*)
+            # do not touch if is empty or looks like an abs path or a URL
+            printf '%s\n' "${1}"
+        ;;
+        .|..|./*|../*|*/*)
+            resolve_relpath_to_abs_if_outside "${1}"
+        ;;
+        *)
+            if [ -L "${1}" ]; then
+                resolve_relpath_to_abs_if_outside "${1}"
+            else
+                printf '%s\n' "${1}"
+            fi
+        ;;
+    esac
+}
+
+# Convert relative paths to absolute if they point outside ${WEBKIT_SOURCE_DIR}
+args_maybe_update_outside_relpaths() {
+    for arg in "${@}"; do
+        case "${arg}" in
+            --*=*)  # handle arguments like "--option=value"
+                key="${arg%%=*}"
+                val="${arg#*=}"
+                val="$(maybe_update_relpath_to_abs_if_outside "${val}")"
+                arg="${key}=${val}"
+                ;;
+            *)
+                arg="$(maybe_update_relpath_to_abs_if_outside "${arg}")"
+                ;;
+        esac
+        printf "'"
+        printf '%s' "${arg}" | sed "s/'/'\\\\''/g"
+        printf "' "
+    done
+}
+
+[ "$#" -ge 1 ] || help
+[ "${1}" = "--help" ] && help
+[ "${1}" = "-h" ] && help
+
+[ -d "${WEBKIT_SOURCE_DIR}" ] || error "Can't find directory: ${WEBKIT_SOURCE_DIR}"
+[ -d /sdk ] || error "Can't find /sdk directory"
+[ -w /sdk ] || error "Directory /sdk is not writable for user $(whoami)"
+
+case "${WEBKIT_SOURCE_DIR}" in
+  /sdk|/sdk/*) error "${0} should not be invoked with WEBKIT_SOURCE_DIR under /sdk directory" ;;
+esac
+
+if [ "${1}" = "--create-symlink" ]; then
+    [ "$#" -ge 2 ] && error "No extra arguments can be passed with --create-symlink"
+    if [ -L "/sdk/webkit" ]; then
+        CURSYMLINKDEST="$(realpath -m -- /sdk/webkit)"
+        if [ "${CURSYMLINKDEST}" != "${WEBKIT_SOURCE_DIR}" ]; then
+            rm -f "/sdk/webkit"
+            ln -s "${WEBKIT_SOURCE_DIR}" "/sdk/webkit"
+            echo "Symlink at /sdk/webkit changed from ${CURSYMLINKDEST} to ${WEBKIT_SOURCE_DIR}"
+        fi
+    elif ! [ -e "/sdk/webkit" ]; then
+        ln -s "${WEBKIT_SOURCE_DIR}" "/sdk/webkit"
+    else
+        error "ERROR: /sdk/webkit exists and is not a symlink. This is unexpected"
+    fi
+    exit 0
+fi
+
+CURUID="$(id -u)"
+CURGID="$(id -g)"
+ORGPWD="$(pwd)"
+SDKPWD="$(maybe_replace_path_prefix_to_rootsdk "${ORGPWD}")"
+# Try also with the realpath, just in case pwd is inside a symlinked dir.
+if [ "${ORGPWD}" = "${SDKPWD}" ]; then
+    ORGPWD="$(realpath -- "${ORGPWD}")"
+    SDKPWD="$(maybe_replace_path_prefix_to_rootsdk "${ORGPWD}")"
+fi
+# When $(pwd) is inside ${WEBKIT_SOURCE_DIR}, switch (cd) to the equivalent bind-mount path
+# under /sdk/webkit. Any relative path arguments that point outside ${WEBKIT_SOURCE_DIR} are
+# converted to absolute paths first, since they wouldn't resolve correctly after the switch.
+[ "${ORGPWD}" != "${SDKPWD}" ] && eval "set -- $(args_maybe_update_outside_relpaths "${@}")"
+
+# Since util-linux 2.27 is not needed to execute mount --make-rprivate /
+# because unshare does that automatically by default for new mount namespaces
+exec unshare -rm sh -c '
+    set -eu
+    WEBKIT_SOURCE_DIR="${1}"
+    SDKPWD="${2}"
+    CURUID="${3}"
+    CURGID="${4}"
+    shift 4
+    mount -t tmpfs -o size=16M tmpfs /sdk
+    mkdir /sdk/webkit
+    mount --bind "${WEBKIT_SOURCE_DIR}" /sdk/webkit
+    cd "${SDKPWD}"
+    export WEBKIT_CONTAINER_SDK_INSIDE_MOUNT_NAMESPACE=1
+    exec unshare --map-user="${CURUID}" --map-group="${CURGID}" -- "${@}"
+' -- "${WEBKIT_SOURCE_DIR}" "${SDKPWD}" "${CURUID}" "${CURGID}" "${@}"

Tools/Scripts/run-bindings-tests
@@ -32,6 +32,8 @@ import optparse
 import os
 import sys
 from webkitpy.common.system import executive
+from webkitpy.port.linux_container_sdk_utils import maybe_use_container_sdk_root_dir
+maybe_use_container_sdk_root_dir()
 
 def main(argv):
     """Runs WebCore bindings code generators on test IDL files and compares

Tools/Scripts/run-builtins-generator-tests
@@ -24,6 +24,8 @@
 
 import sys
 from webkitpy.common.system import executive
+from webkitpy.port.linux_container_sdk_utils import maybe_use_container_sdk_root_dir
+maybe_use_container_sdk_root_dir()
 
 def main(argv):
     """Runs the JS builtins code generator on test input files and compares

Tools/Scripts/run-css-property-code-generation-tests
@@ -32,6 +32,8 @@ import optparse
 import os
 import sys
 from webkitpy.common.system import executive
+from webkitpy.port.linux_container_sdk_utils import maybe_use_container_sdk_root_dir
+maybe_use_container_sdk_root_dir()
 
 def main(argv):
     """Runs WebCore css property code generator on test input and compares

commit 26d98b8bbb4953abcc70ff8963dccb193b5f94bd

[Tools][WPE][GTK][SDK] Regression from 304672@main: cannot find jsc binary in webkitperl tests https://bugs.webkit.org/show_bug.cgi?id=304614
Reviewed by Fujii Hironori.

The maybeUseContainerSDKRootDir() function was being called inside setupEnvironment(),
which runs after processCLI(). By that time, GetOptions had consumed all command-line
arguments from @ARGV. When the SDK wrapper re-executed the script inside the mount
namespace, it passed an empty/modified @ARGV, losing arguments like --jsc, --release, etc.

Move maybeUseContainerSDKRootDir() to be called at the start of main() before
processCLI(), ensuring @ARGV still contains all original arguments when re-exec happens.
This matches the pattern used by run-javascriptcore-tests and other scripts.

Covered by existing webkitperl tests.

* Tools/Scripts/test262/Runner.pm:
(setupEnvironment):
(main):

Canonical link: https://commits.webkit.org/305101@main

Tools/Scripts/test262/Runner.pm
@@ -323,12 +323,14 @@ sub processCLI {
 sub setupEnvironment()
 {
     if ($^O eq "linux") {
-        maybeUseContainerSDKRootDir();
         setupUnixWebKitEnvironment(productDir());
     }
 }
 
 sub main {
+    if ($^O eq "linux") {
+        maybeUseContainerSDKRootDir();
+    }
     processCLI();
     setupEnvironment();
 