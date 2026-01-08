szewai/WebKit

commit 93ddb5c2ffabbf8d106482aa0a3fa71d7d34fef4

ANGLE: Avoid using UNPACK_SKIP_IMAGES, UNPACK_IMAGE_HEIGHT for non-3d textures <rdar://164155603>
Reviewed by Mike Wyrzykowski.

Avoid using UNPACK_SKIP_IMAGES, UNPACK_IMAGE_HEIGHT when uploading
textures with TexImage2D. The UNPACK_IMAGE_HEIGHT was being
being used in Metal texture uploads even when slices was == 1. This
causes validation failure. During TexImage2D the values are not
validated at ANGLE validation level since they are not supposed to
affect anything.

Consider pattern:
formatInfo.computeRowPitch(type, area.width, unpack.alignment,
    unpack.rowLength, &rowPitch);
formatInfo.computeDepthPitch(area.height, unpack.imageHeight,
    sourceRowPitch, &depthPitch);
formatInfo.computeSkipBytes(type, sourceRowPitch, sourceDepthPitch,
    unpack, index.usesTex3D(), &sourceSkipBytes);

Computing the depthPitch would not take into account wheter the
originating command was Tex*Image2D or Tex*Image3D. It would
unconditionally use UNPACK_SKIP_IMAGES, UNPACK_IMAGE_HEIGHT.
The computed depthPitch would then be used to size buffers, read amounts
and write amounts by the implementations.

Combine the pattern to InternalFormat::computeRowDepthSkipBytes(...)
call that uses PixelUnpackState::imageHeight,
PixelUnpackState::imageSkip only for 3D texture uploads.

Compine the pack buffer parameter resolution logic other to function:
InternalFormat::computeRowSkipBytes(..., PixelPackState&,...).

* Source/ThirdParty/ANGLE/src/libANGLE/Context.cpp:
(gl::Context::texImage2D):
* Source/ThirdParty/ANGLE/src/libANGLE/capture/capture_gles_2_0_params.cpp:
(gl::CaptureTexImage2D_pixels):
* Source/ThirdParty/ANGLE/src/libANGLE/formatutils.cpp:
(gl::InternalFormat::computeRowDepthSkipBytes const):
(gl::InternalFormat::computeSkipBytes const):
(gl::InternalFormat::computePackUnpackEndByte const):
* Source/ThirdParty/ANGLE/src/libANGLE/formatutils.h:
* Source/ThirdParty/ANGLE/src/libANGLE/renderer/d3d/d3d11/Image11.cpp:
(rx::Image11::loadData):

Source/ThirdParty/ANGLE/src/libANGLE/formatutils.cpp
@@ -1753,6 +1753,64 @@ bool InternalFormat::computePalettedImageRowPitch(GLsizei width, GLuint *resultO
     }
 }
 
+bool InternalFormat::computeRowDepthSkipBytes(GLenum formatType,
+                                              const Extents &area,
+                                              const gl::PixelStoreStateBase &unpack,
+                                              bool is3D,
+                                              GLuint *rowPitchOut,
+                                              GLuint *depthPitchOut,
+                                              GLuint *skipBytesOut) const
+{
+    GLuint rowPitch = 0;
+    if (!computeRowPitch(type, area.width, unpack.alignment, unpack.rowLength, &rowPitch))
+    {
+        return false;
+    }
+    GLuint depthPitch       = 0;
+    const GLint imageHeight = is3D ? unpack.imageHeight : 0;
+    if (!computeDepthPitch(area.height, imageHeight, rowPitch, &depthPitch))
+    {
+        return false;
+    }
+    GLuint skipBytes        = 0;
+    const GLuint skipRows   = static_cast<GLuint>(unpack.skipRows);
+    const GLuint skipPixels = static_cast<GLuint>(unpack.skipPixels);
+    const GLuint skipImages = is3D ? static_cast<GLuint>(unpack.skipImages) : 0u;
+    if (!computeSkipBytes(type, rowPitch, depthPitch, skipRows, skipPixels, skipImages, &skipBytes))
+    {
+        return false;
+    }
+    *rowPitchOut   = rowPitch;
+    *depthPitchOut = depthPitch;
+    *skipBytesOut  = skipBytes;
+    return true;
+}
+
+bool InternalFormat::computeRowSkipBytes(GLenum formatType,
+                                         GLsizei width,
+                                         const gl::PixelPackState &pack,
+                                         GLuint *rowPitchOut,
+                                         GLuint *skipBytesOut) const
+{
+    GLuint rowPitch = 0;
+    if (!computeRowPitch(type, width, pack.alignment, pack.rowLength, &rowPitch))
+    {
+        return false;
+    }
+    GLuint skipBytes        = 0;
+    const GLuint depthPitch = 0;
+    const GLuint skipRows   = static_cast<GLuint>(pack.skipRows);
+    const GLuint skipPixels = static_cast<GLuint>(pack.skipPixels);
+    const GLuint skipImages = 0u;
+    if (!computeSkipBytes(type, rowPitch, depthPitch, skipRows, skipPixels, skipImages, &skipBytes))
+    {
+        return false;
+    }
+    *rowPitchOut  = rowPitch;
+    *skipBytesOut = skipBytes;
+    return true;
+}
+
 bool InternalFormat::computeRowPitch(GLenum formatType,
                                      GLsizei width,
                                      GLint alignment,
@@ -1923,23 +1981,14 @@ std::pair<GLuint, GLuint> InternalFormat::getCompressedImageMinBlocks() const
 bool InternalFormat::computeSkipBytes(GLenum formatType,
                                       GLuint rowPitch,
                                       GLuint depthPitch,
-                                      const PixelStoreStateBase &state,
-                                      bool is3D,
+                                      GLuint skipRows,
+                                      GLuint skipPixels,
+                                      GLuint skipImages,
                                       GLuint *resultOut) const
 {
-    CheckedNumeric<GLuint> checkedRowPitch(rowPitch);
-    CheckedNumeric<GLuint> checkedDepthPitch(depthPitch);
-    CheckedNumeric<GLuint> checkedSkipImages(static_cast<GLuint>(state.skipImages));
-    CheckedNumeric<GLuint> checkedSkipRows(static_cast<GLuint>(state.skipRows));
-    CheckedNumeric<GLuint> checkedSkipPixels(static_cast<GLuint>(state.skipPixels));
-    CheckedNumeric<GLuint> checkedPixelBytes(computePixelBytes(formatType));
-    auto checkedSkipImagesBytes = checkedSkipImages * checkedDepthPitch;
-    if (!is3D)
-    {
-        checkedSkipImagesBytes = 0;
-    }
-    auto skipBytes = checkedSkipImagesBytes + checkedSkipRows * checkedRowPitch +
-                     checkedSkipPixels * checkedPixelBytes;
+    auto skipBytes = CheckedNumeric<GLuint>{skipImages} * depthPitch +
+                     CheckedNumeric<GLuint>{skipRows} * rowPitch +
+                     CheckedNumeric<GLuint>{skipPixels} * computePixelBytes(formatType);
     return CheckedMathResult(skipBytes, resultOut);
 }
 
@@ -1950,13 +1999,10 @@ bool InternalFormat::computePackUnpackEndByte(GLenum formatType,
                                               GLuint *resultOut) const
 {
     GLuint rowPitch = 0;
-    if (!computeRowPitch(formatType, size.width, state.alignment, state.rowLength, &rowPitch))
-    {
-        return false;
-    }
-
     GLuint depthPitch = 0;
-    if (is3D && !computeDepthPitch(size.height, state.imageHeight, rowPitch, &depthPitch))
+    GLuint skipBytes  = 0;
+    if (!computeRowDepthSkipBytes(formatType, size, state, is3D, &rowPitch, &depthPitch,
+                                  &skipBytes))
     {
         return false;
     }
@@ -1984,12 +2030,6 @@ bool InternalFormat::computePackUnpackEndByte(GLenum formatType,
             CheckedNumeric<GLuint> depthMinusOne = size.depth - 1;
             checkedCopyBytes += depthMinusOne * depthPitch;
         }
-    }
-
-    GLuint skipBytes = 0;
-    if (!computeSkipBytes(formatType, rowPitch, depthPitch, state, is3D, &skipBytes))
-    {
-        return false;
     }
 
     CheckedNumeric<GLuint> endByte = checkedCopyBytes + CheckedNumeric<GLuint>(skipBytes);

Source/ThirdParty/ANGLE/src/libANGLE/capture/capture_gles_2_0_params.cpp
@@ -714,16 +714,12 @@ void CaptureTexImage2D_pixels(const State &glState,
 
     const gl::InternalFormat &internalFormatInfo = gl::GetInternalFormatInfo(format, type);
     const gl::PixelUnpackState &unpack           = glState.getUnpackState();
-
     GLuint srcRowPitch = 0;
-    (void)internalFormatInfo.computeRowPitch(type, width, unpack.alignment, unpack.rowLength,
-                                             &srcRowPitch);
     GLuint srcDepthPitch = 0;
-    (void)internalFormatInfo.computeDepthPitch(height, unpack.imageHeight, srcRowPitch,
-                                               &srcDepthPitch);
     GLuint srcSkipBytes = 0;
-    (void)internalFormatInfo.computeSkipBytes(type, srcRowPitch, srcDepthPitch, unpack, false,
-                                              &srcSkipBytes);
+    (void)internalFormatInfo.computeRowDepthSkipBytes(type, gl::Extents{width, height, 0}, unpack,
+                                                      false, &srcRowPitch, &srcDepthPitch,
+                                                      &srcSkipBytes);
 
     // For the last row of pixels, we don't round up to the unpack alignment. This often affects
     // 1x1 sized textures because they may be 1 or 2 bytes wide with an alignment of 4 bytes.

Source/ThirdParty/ANGLE/src/libANGLE/formatutils.h
@@ -180,7 +180,20 @@ struct InternalFormat
 
     [[nodiscard]] bool computeBufferRowLength(uint32_t width, uint32_t *resultOut) const;
     [[nodiscard]] bool computeBufferImageHeight(uint32_t height, uint32_t *resultOut) const;
-
+    // FIXME: When computePackUnpackEndByte does not call this, change PixelStoreStateBase
+    // to glPixelUnpackState.
+    [[nodiscard]] bool computeRowDepthSkipBytes(GLenum formatType,
+                                                const Extents &size,
+                                                const gl::PixelStoreStateBase &unpack,
+                                                bool is3D,
+                                                GLuint *rowPitchOut,
+                                                GLuint *depthPitchOut,
+                                                GLuint *skipBytesOut) const;
+    [[nodiscard]] bool computeRowSkipBytes(GLenum formatType,
+                                           GLsizei width,
+                                           const gl::PixelPackState &pack,
+                                           GLuint *rowPitchOut,
+                                           GLuint *skipBytesOut) const;
     [[nodiscard]] bool computeRowPitch(GLenum formatType,
                                        GLsizei width,
                                        GLint alignment,
@@ -209,12 +222,12 @@ struct InternalFormat
     [[nodiscard]] bool computeCompressedImageSize(const Extents &size, GLuint *resultOut) const;
 
     [[nodiscard]] std::pair<GLuint, GLuint> getCompressedImageMinBlocks() const;
-
     [[nodiscard]] bool computeSkipBytes(GLenum formatType,
                                         GLuint rowPitch,
                                         GLuint depthPitch,
-                                        const PixelStoreStateBase &state,
-                                        bool is3D,
+                                        GLuint skipRows,
+                                        GLuint skipPixels,
+                                        GLuint skipImages,
                                         GLuint *resultOut) const;
 
     [[nodiscard]] bool computePackUnpackEndByte(GLenum formatType,

Source/ThirdParty/ANGLE/src/libANGLE/renderer/d3d/FramebufferD3D.cpp
@@ -224,13 +224,10 @@ angle::Result FramebufferD3D::readPixels(const gl::Context *context,
     ContextD3D *contextD3D = GetImplAs<ContextD3D>(context);
 
     GLuint outputPitch = 0;
-    ANGLE_CHECK_GL_MATH(contextD3D,
-                        sizedFormatInfo.computeRowPitch(type, area.width, pack.alignment,
-                                                        pack.rowLength, &outputPitch));
-
     GLuint outputSkipBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextD3D, sizedFormatInfo.computeSkipBytes(type, outputPitch, 0, pack,
-                                                                     false, &outputSkipBytes));
+
+    ANGLE_CHECK_GL_MATH(contextD3D, sizedFormatInfo.computeRowSkipBytes(
+                                        type, area.width, pack, &outputPitch, &outputSkipBytes));
     outputSkipBytes += (clippedArea.x - area.x) * sizedFormatInfo.pixelBytes +
                        (clippedArea.y - area.y) * outputPitch;
 

Source/ThirdParty/ANGLE/src/libANGLE/renderer/d3d/d3d11/Image11.cpp
@@ -281,16 +281,14 @@ angle::Result Image11::loadData(const gl::Context *context,
     Context11 *context11 = GetImplAs<Context11>(context);
 
     const gl::InternalFormat &formatInfo = gl::GetSizedInternalFormatInfo(mInternalFormat);
-    GLuint inputRowPitch                 = 0;
-    ANGLE_CHECK_GL_MATH(context11, formatInfo.computeRowPitch(type, area.width, unpack.alignment,
-                                                              unpack.rowLength, &inputRowPitch));
+
+    GLuint inputRowPitch   = 0;
     GLuint inputDepthPitch = 0;
-    ANGLE_CHECK_GL_MATH(context11, formatInfo.computeDepthPitch(area.height, unpack.imageHeight,
-                                                                inputRowPitch, &inputDepthPitch));
     GLuint inputSkipBytes = 0;
     ANGLE_CHECK_GL_MATH(context11,
-                        formatInfo.computeSkipBytes(type, inputRowPitch, inputDepthPitch, unpack,
-                                                    applySkipImages, &inputSkipBytes));
+                        formatInfo.computeRowDepthSkipBytes(
+                            type, gl::Extents{area.width, area.height, area.depth}, unpack,
+                            applySkipImages, &inputRowPitch, &inputDepthPitch, &inputSkipBytes));
 
     const d3d11::DXGIFormatSize &dxgiFormatInfo = d3d11::GetDXGIFormatSizeInfo(mDXGIFormat);
     GLuint outputPixelSize                      = dxgiFormatInfo.pixelBytes;

Source/ThirdParty/ANGLE/src/libANGLE/renderer/d3d/d3d11/TextureStorage11.cpp
@@ -904,20 +904,14 @@ angle::Result TextureStorage11::setData(const gl::Context *context,
 
     Context11 *context11 = GetImplAs<Context11>(context);
 
-    const int width    = destBox ? destBox->width : static_cast<int>(image->getWidth());
-    const int height   = destBox ? destBox->height : static_cast<int>(image->getHeight());
-    const int depth    = destBox ? destBox->depth : static_cast<int>(image->getDepth());
+    const gl::Box area = destBox ? *destBox : gl::Box{static_cast<int>(image->getWidth(), static_cast<int>(image->getHeight()), static_cast<int>(image->getDepth())};
     GLuint srcRowPitch = 0;
-    ANGLE_CHECK_GL_MATH(context11,
-                        internalFormatInfo.computeRowPitch(type, width, unpack.alignment,
-                                                           unpack.rowLength, &srcRowPitch));
     GLuint srcDepthPitch = 0;
-    ANGLE_CHECK_GL_MATH(context11, internalFormatInfo.computeDepthPitch(
-                                       height, unpack.imageHeight, srcRowPitch, &srcDepthPitch));
     GLuint srcSkipBytes = 0;
-    ANGLE_CHECK_GL_MATH(
-        context11, internalFormatInfo.computeSkipBytes(type, srcRowPitch, srcDepthPitch, unpack,
-                                                       index.usesTex3D(), &srcSkipBytes));
+
+    ANGLE_CHECK_GL_MATH(context11, internalFormatInfo.computeRowDepthSkipBytes(
+                                       type, area, unpack, index.usesTex3D(), &srcRowPitch,
+                                       &srcDepthPitch, &srcSkipBytes));
 
     const d3d11::Format &d3d11Format =
         d3d11::Format::Get(image->getInternalFormat(), mRenderer->getRenderer11DeviceCaps());

Source/ThirdParty/ANGLE/src/libANGLE/renderer/gl/FramebufferGL.cpp
@@ -362,13 +362,10 @@ angle::Result RearrangeEXTTextureNorm16Pixels(const gl::Context *context,
         gl::GetInternalFormatInfo(originalReadFormat, type);
 
     GLuint originalReadFormatRowBytes = 0;
-    ANGLE_CHECK_GL_MATH(
-        contextGL, glFormatOriginal.computeRowPitch(type, area.width, pack.alignment,
-                                                    pack.rowLength, &originalReadFormatRowBytes));
     GLuint originalReadFormatSkipBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL,
-                        glFormatOriginal.computeSkipBytes(type, originalReadFormatRowBytes, 0, pack,
-                                                          false, &originalReadFormatSkipBytes));
+    ANGLE_CHECK_GL_MATH(contextGL, glFormatOriginal.computeRowSkipBytes(
+                                       type, area.width, pack, &originalReadFormatRowBytes,
+                                       &originalReadFormatSkipBytes));
 
     GLuint originalReadFormatPixelBytes = glFormatOriginal.computePixelBytes(type);
     GLuint alphaChannelBytes            = glFormatOriginal.alphaBits / 8;
@@ -1595,11 +1592,9 @@ angle::Result FramebufferGL::readPixelsRowByRow(const gl::Context *context,
     const gl::InternalFormat &glFormat = gl::GetInternalFormatInfo(format, type);
 
     GLuint rowBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeRowPitch(type, area.width, pack.alignment,
-                                                            pack.rowLength, &rowBytes));
     GLuint skipBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL,
-                        glFormat.computeSkipBytes(type, rowBytes, 0, pack, false, &skipBytes));
+    ANGLE_CHECK_GL_MATH(
+        contextGL, glFormat.computeRowSkipBytes(type, area.width, pack, &rowBytes, &skipBytes));
 
     ScopedEXTTextureNorm16ReadbackWorkaround workaround;
     angle::Result result =
@@ -1650,11 +1645,9 @@ angle::Result FramebufferGL::readPixelsAllAtOnce(const gl::Context *context,
     const gl::InternalFormat &glFormat = gl::GetInternalFormatInfo(format, type);
 
     GLuint rowBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeRowPitch(type, area.width, pack.alignment,
-                                                            pack.rowLength, &rowBytes));
     GLuint skipBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL,
-                        glFormat.computeSkipBytes(type, rowBytes, 0, pack, false, &skipBytes));
+    ANGLE_CHECK_GL_MATH(
+        contextGL, glFormat.computeRowSkipBytes(type, area.width, pack, &rowBytes, &skipBytes));
 
     ScopedEXTTextureNorm16ReadbackWorkaround workaround;
     angle::Result result =

Source/ThirdParty/ANGLE/src/libANGLE/renderer/gl/TextureGL.cpp
@@ -443,17 +443,15 @@ angle::Result TextureGL::setSubImageRowByRowWorkaround(const gl::Context *contex
     ANGLE_TRY(stateManager->setPixelUnpackBuffer(context, unpackBuffer));
 
     const gl::InternalFormat &glFormat = gl::GetInternalFormatInfo(format, type);
-    GLuint rowBytes                    = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeRowPitch(type, area.width, unpack.alignment,
-                                                            unpack.rowLength, &rowBytes));
-    GLuint imageBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeDepthPitch(area.height, unpack.imageHeight,
-                                                              rowBytes, &imageBytes));
 
-    bool useTexImage3D = nativegl::UseTexImage3D(getType());
+    GLuint rowBytes    = 0;
+    GLuint imageBytes  = 0;
     GLuint skipBytes   = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeSkipBytes(type, rowBytes, imageBytes, unpack,
-                                                             useTexImage3D, &skipBytes));
+    bool useTexImage3D = nativegl::UseTexImage3D(getType());
+
+    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeRowDepthSkipBytes(
+                                       type, gl::Extents{area.width, area.height, area.depth},
+                                       unpack, useTexImage3D, &rowBytes, &imageBytes, &skipBytes));
 
     GLint rowsPerChunk =
         std::min(std::max(static_cast<GLint>(maxBytesUploadedPerChunk / rowBytes), 1), area.height);
@@ -518,15 +516,13 @@ angle::Result TextureGL::setSubImagePaddingWorkaround(const gl::Context *context
 
     const gl::InternalFormat &glFormat = gl::GetInternalFormatInfo(format, type);
     GLuint rowBytes                    = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeRowPitch(type, area.width, unpack.alignment,
-                                                            unpack.rowLength, &rowBytes));
-    GLuint imageBytes = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeDepthPitch(area.height, unpack.imageHeight,
-                                                              rowBytes, &imageBytes));
+    GLuint imageBytes                  = 0;
     bool useTexImage3D = nativegl::UseTexImage3D(getType());
     GLuint skipBytes   = 0;
-    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeSkipBytes(type, rowBytes, imageBytes, unpack,
-                                                             useTexImage3D, &skipBytes));
+
+    ANGLE_CHECK_GL_MATH(contextGL, glFormat.computeRowDepthSkipBytes(
+                                       type, gl::Extents{area.width, area.height, area.depth},
+                                       unpack, useTexImage3D, &rowBytes, &imageBytes, &skipBytes));
 
     ANGLE_TRY(stateManager->setPixelUnpackState(context, unpack));
     ANGLE_TRY(stateManager->setPixelUnpackBuffer(context, unpackBuffer));

commit d5114b38ca89d9cbdfdddb71937a71a15e80ff08

[Win] Fix WebGL regression after 304611@main https://bugs.webkit.org/show_bug.cgi?id=305073
Reviewed by Yusuke Suzuki.

Incorrectly using `type` from the InternalFormat struct, instead of the
passed parameter `formatType`.

Canonical link: https://commits.webkit.org/305288@main

Source/ThirdParty/ANGLE/src/libANGLE/formatutils.cpp
@@ -1762,7 +1762,7 @@ bool InternalFormat::computeRowDepthSkipBytes(GLenum formatType,
                                               GLuint *skipBytesOut) const
 {
     GLuint rowPitch = 0;
-    if (!computeRowPitch(type, area.width, unpack.alignment, unpack.rowLength, &rowPitch))
+    if (!computeRowPitch(formatType, area.width, unpack.alignment, unpack.rowLength, &rowPitch))
     {
         return false;
     }
@@ -1776,7 +1776,7 @@ bool InternalFormat::computeRowDepthSkipBytes(GLenum formatType,
     const GLuint skipRows   = static_cast<GLuint>(unpack.skipRows);
     const GLuint skipPixels = static_cast<GLuint>(unpack.skipPixels);
     const GLuint skipImages = is3D ? static_cast<GLuint>(unpack.skipImages) : 0u;
-    if (!computeSkipBytes(type, rowPitch, depthPitch, skipRows, skipPixels, skipImages, &skipBytes))
+    if (!computeSkipBytes(formatType, rowPitch, depthPitch, skipRows, skipPixels, skipImages, &skipBytes))
     {
         return false;
     }
@@ -1793,7 +1793,7 @@ bool InternalFormat::computeRowSkipBytes(GLenum formatType,
                                          GLuint *skipBytesOut) const
 {
     GLuint rowPitch = 0;
-    if (!computeRowPitch(type, width, pack.alignment, pack.rowLength, &rowPitch))
+    if (!computeRowPitch(formatType, width, pack.alignment, pack.rowLength, &rowPitch))
     {
         return false;
     }
@@ -1802,7 +1802,7 @@ bool InternalFormat::computeRowSkipBytes(GLenum formatType,
     const GLuint skipRows   = static_cast<GLuint>(pack.skipRows);
     const GLuint skipPixels = static_cast<GLuint>(pack.skipPixels);
     const GLuint skipImages = 0u;
-    if (!computeSkipBytes(type, rowPitch, depthPitch, skipRows, skipPixels, skipImages, &skipBytes))
+    if (!computeSkipBytes(formatType, rowPitch, depthPitch, skipRows, skipPixels, skipImages, &skipBytes))
     {
         return false;
     }