szewai/WebKit

commit 21b3626e181e759a3202bdc3248cce2549f39acf

[Site Isolation] Cross-site iframes don't receive DeviceOrientationEvents https://bugs.webkit.org/show_bug.cgi?id=303618 rdar://165900470
Reviewed by Brady Eidson.

With site isolation on, cross-site iframes don't receive DeviceOrientationEvents.
There are mulitple problems we must fix to get this working:

1. Not listening for the events:

    Even if the cross-site iframe is granted permission to receive the events
    and has registered an event listener, WebKit still won't listen for events
    from CoreMotion.

    The Web Process tells the UI Process to listen for events from CoreMotion
    via this call chain:

    1. Event listener is added.
    2. The Web Process sends an IPC to the UI Process in
       WebDeviceOrientationUpdateProvider::startUpdatingDeviceOrientation()
    3. WebDeviceOrientationUpdateProviderProxy::StartUpdatingDeviceOrientation().
       is called on the UI Process side, and then the UI Process then starts
       listening for events from CoreMotion.

    In the case of the cross-site iframe, the UI Process never received this
    message. This is because, as we see from it's constructor,
    WebDeviceOrientationUpdateProviderProxy is only set up to receiver messages from
    the main frame's Web Process.

    To allow WebDeviceOrientationUpdateProviderProxy to receive messages from any
    cross-site Web Process (even those created in the future), we modify
    RemotePageProxy (which is created for every cross-site Web Process) to ensure
    that WebDeviceOrientationUpdateProviderProxy can receive messages from the
    Web Process associated with that RemotePageProxy.

2. Not sending the events

    So now the cross-site iframe's Web Process is listening for the events, but
    the UI Process is not sending them. We see in
    WebDeviceOrientationUpdateProviderProxy::orientationChanged that the events
    are sent only to the main frame's Web Process.

    We fix this to ensure that events are sent to all Web Processes.

3. Testing:

Source/WebKit/UIProcess/RemotePageProxy.cpp
@@ -63,6 +63,10 @@
 #include "RemotePageVideoPresentationManagerProxy.h"
 #endif
 
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+#include "WebDeviceOrientationUpdateProviderProxy.h"
+#endif
+
 namespace WebKit {
 
 WTF_MAKE_TZONE_ALLOCATED_IMPL(RemotePageProxy);
@@ -85,6 +89,10 @@ RemotePageProxy::RemotePageProxy(WebPageProxy& page, WebProcessProxy& process, c
         m_messageReceiverRegistration.startReceivingMessages(m_process, m_webPageID, *this, page.backForwardList());
 
     m_process->addRemotePageProxy(*this);
+
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    m_page->webDeviceOrientationUpdateProviderProxy()->addAsMessageReceiverForProcess(m_process.get(), m_webPageID);
+#endif
 }
 
 void RemotePageProxy::injectPageIntoNewProcess()
@@ -151,6 +159,10 @@ void RemotePageProxy::processDidTerminate(WebProcessProxy& process, ProcessTermi
 
 RemotePageProxy::~RemotePageProxy()
 {
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    m_page->webDeviceOrientationUpdateProviderProxy()->removeAsMessageReceiverForProcess(m_process.get(), m_webPageID);
+#endif
+
     if (RefPtr page = m_page.get())
         page->isNoLongerAssociatedWithRemotePage(*this);
     if (m_drawingArea)

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -15596,7 +15596,9 @@ void WebPageProxy::willAcquireUniversalFileReadSandboxExtension(WebProcessProxy&
 
 void WebPageProxy::simulateDeviceOrientationChange(double alpha, double beta, double gamma)
 {
-    send(Messages::WebPage::SimulateDeviceOrientationChange(alpha, beta, gamma));
+    forEachWebContentProcess([&](auto& webProcess, auto pageID) {
+        webProcess.send(Messages::WebPage::SimulateDeviceOrientationChange(alpha, beta, gamma), pageID);
+    });
 }
 
 #if ENABLE(DATA_DETECTION)
@@ -17401,6 +17403,13 @@ bool shouldShowSwiftDemoLogo()
     RELEASE_ASSERT_NOT_REACHED();
 #endif
 }
+
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+RefPtr<WebDeviceOrientationUpdateProviderProxy> WebPageProxy::webDeviceOrientationUpdateProviderProxy()
+{
+    return m_webDeviceOrientationUpdateProviderProxy;
+}
+#endif
 
 } // namespace WebKit
 

Source/WebKit/UIProcess/WebPageProxy.h
@@ -2875,6 +2875,10 @@ public:
     void dropNetworkActivity();
     bool hasValidNetworkActivity() const;
 
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    RefPtr<WebDeviceOrientationUpdateProviderProxy> webDeviceOrientationUpdateProviderProxy();
+#endif
+
 private:
     WebPageProxy(PageClient&, WebProcessProxy&, Ref<API::PageConfiguration>&&);
     void platformInitialize();

Source/WebKit/UIProcess/ios/WebDeviceOrientationUpdateProviderProxy.h
@@ -29,6 +29,7 @@
 
 #include "MessageReceiver.h"
 #include <WebCore/MotionManagerClient.h>
+#include <WebCore/PageIdentifier.h>
 #include <wtf/CheckedRef.h>
 #include <wtf/TZoneMalloc.h>
 
@@ -38,6 +39,7 @@ class SecurityOriginData;
 namespace WebKit {
 
 class WebPageProxy;
+class WebProcessProxy;
 struct SharedPreferencesForWebProcess;
 
 class WebDeviceOrientationUpdateProviderProxy final : public WebCore::MotionManagerClient, private IPC::MessageReceiver, public RefCounted<WebDeviceOrientationUpdateProviderProxy>, public CanMakeCheckedPtr<WebDeviceOrientationUpdateProviderProxy> {
@@ -64,6 +66,9 @@ public:
     void incrementCheckedPtrCount() const final { CanMakeCheckedPtr::incrementCheckedPtrCount(); }
     void decrementCheckedPtrCount() const final { CanMakeCheckedPtr::decrementCheckedPtrCount(); }
     void setDidBeginCheckedPtrDeletion() final { CanMakeCheckedPtr::setDidBeginCheckedPtrDeletion(); }
+
+    void addAsMessageReceiverForProcess(WebProcessProxy&, WebCore::PageIdentifier);
+    void removeAsMessageReceiverForProcess(WebProcessProxy&, WebCore::PageIdentifier);
 
 private:
     explicit WebDeviceOrientationUpdateProviderProxy(WebPageProxy&);

Source/WebKit/UIProcess/ios/WebDeviceOrientationUpdateProviderProxy.mm
@@ -57,6 +57,16 @@ WebDeviceOrientationUpdateProviderProxy::~WebDeviceOrientationUpdateProviderProx
         page->protectedLegacyMainFrameProcess()->removeMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), page->webPageIDInMainFrameProcess());
 }
 
+void WebDeviceOrientationUpdateProviderProxy::addAsMessageReceiverForProcess(WebProcessProxy& process, WebCore::PageIdentifier pageID)
+{
+    process.addMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), pageID, *this);
+}
+
+void WebDeviceOrientationUpdateProviderProxy::removeAsMessageReceiverForProcess(WebProcessProxy& process, WebCore::PageIdentifier pageID)
+{
+    process.removeMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), pageID);
+}
+
 void WebDeviceOrientationUpdateProviderProxy::startUpdatingDeviceOrientation()
 {
     [[WebCoreMotionManager sharedManager] addOrientationClient:this];
@@ -79,8 +89,11 @@ void WebDeviceOrientationUpdateProviderProxy::stopUpdatingDeviceMotion()
 
 void WebDeviceOrientationUpdateProviderProxy::orientationChanged(double alpha, double beta, double gamma, double compassHeading, double compassAccuracy)
 {
-    if (RefPtr page = m_page.get())
-        page->protectedLegacyMainFrameProcess()->send(Messages::WebDeviceOrientationUpdateProvider::DeviceOrientationChanged(alpha, beta, gamma, compassHeading, compassAccuracy), m_page->webPageIDInMainFrameProcess());
+    if (RefPtr page = m_page.get()) {
+        page->forEachWebContentProcess([&](auto& webProcess, auto pageID) {
+            webProcess.send(Messages::WebDeviceOrientationUpdateProvider::DeviceOrientationChanged(alpha, beta, gamma, compassHeading, compassAccuracy), pageID);
+        });
+    }
 }
 
 void WebDeviceOrientationUpdateProviderProxy::motionChanged(double xAcceleration, double yAcceleration, double zAcceleration, double xAccelerationIncludingGravity, double yAccelerationIncludingGravity, double zAccelerationIncludingGravity, std::optional<double> xRotationRate, std::optional<double> yRotationRate, std::optional<double> zRotationRate)

Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -8472,8 +8472,14 @@ WebCore::DOMPasteAccessResponse WebPage::requestDOMPasteAccess(DOMPasteAccessCat
 void WebPage::simulateDeviceOrientationChange(double alpha, double beta, double gamma)
 {
 #if ENABLE(DEVICE_ORIENTATION) && PLATFORM(IOS_FAMILY)
-    if (RefPtr localTopDocument = this->localTopDocument())
-        localTopDocument->simulateDeviceOrientationChange(alpha, beta, gamma);
+    for (RefPtr frame = mainFrame(); frame; frame = frame->tree().traverseNext()) {
+        RefPtr localFrame = dynamicDowncast<LocalFrame>(frame.get());
+        if (!localFrame)
+            continue;
+
+        if (RefPtr document = localFrame->document())
+            document->simulateDeviceOrientationChange(alpha, beta, gamma);
+    }
 #endif
 }
 

Tools/TestWebKitAPI/Tests/WebKitCocoa/SiteIsolation.mm
@@ -7448,4 +7448,58 @@ TEST(SiteIsolation, BrowsingContextGroupSwitchForIncompatibleCrossOriginOpenerPo
     });
 }
 
+#if ENABLE(DEVICE_ORIENTATION) && PLATFORM(IOS_FAMILY)
+
+TEST(SiteIsolation, CrossSiteIFrameCanReceiveDeviceOrientationEvents)
+{
+    auto mainframeHTML = "<iframe src='https://examplesubframe.com/subframe' allow='accelerometer;gyroscope;magnetometer'></iframe>"_s;
+
+    auto subframeHTML = "<script>"
+        "    window.addEventListener('deviceorientation', function(event) {"
+        "        alert('deviceOrientationEvent received');"
+        "    });"
+        "    "
+        "    window.addEventListener('message', function(event) {"
+        "        if (event.data === 'requestPermission') {"
+        "            DeviceOrientationEvent.requestPermission().then(function(result) {"
+        "                alert('permission granted');"
+        "            }).catch(function(error) {"
+        "                alert('error getting permission');"
+        "            });"
+        "        }"
+        "    });"
+        "    "
+        "    "
+        "    alert('iframe loaded');"
+        "</script>"_s;
+
+    HTTPServer server({
+        { "/mainframe"_s, { mainframeHTML } },
+        { "/subframe"_s, { subframeHTML } }
+    }, HTTPServer::Protocol::HttpsProxy);
+
+    auto [webView, navigationDelegate] = siteIsolatedViewAndDelegate(server);
+
+    __block bool askedClientForPermission = false;
+
+    RetainPtr uiDelegate = adoptNS([TestUIDelegate new]);
+    [uiDelegate setRequestDeviceOrientationAndMotionPermissionForOrigin:^(WKSecurityOrigin *, WKFrameInfo *, void (^completion)(WKPermissionDecision)) {
+        askedClientForPermission = true;
+        completion(WKPermissionDecisionGrant);
+    }];
+    [webView setUIDelegate:uiDelegate.get()];
+
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://examplemainframe.com/mainframe"]]];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "iframe loaded");
+
+    [webView evaluateJavaScript:@"DeviceOrientationEvent.requestPermission()" inFrame:[webView firstChildFrame] completionHandler:nil];
+    TestWebKitAPI::Util::run(&askedClientForPermission);
+    askedClientForPermission = false;
+
+    [webView _simulateDeviceOrientationChangeWithAlpha:45.0 beta:90.0 gamma:180.0];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "deviceOrientationEvent received");
+}
+
+#endif // ENABLE(DEVICE_ORIENTATION) && PLATFORM(IOS_FAMILY)
+
 }

Tools/TestWebKitAPI/cocoa/TestUIDelegate.h
@@ -52,6 +52,10 @@
 @property (nonatomic, copy) void (^insertInputSuggestion)(WKWebView *, UIInputSuggestion *);
 #endif
 
+#if ENABLE(DEVICE_ORIENTATION) && PLATFORM(IOS_FAMILY)
+@property (nonatomic, copy) void (^requestDeviceOrientationAndMotionPermissionForOrigin)(WKSecurityOrigin *, WKFrameInfo *, void(^)(WKPermissionDecision));
+#endif
+
 - (NSString *)waitForAlert;
 - (NSString *)waitForConfirm;
 - (NSString *)waitForPromptWithReply:(NSString *)reply;

commit 3faa7e4c4945b792290462314a65a3a906f016ac

[Site Isolation] REGRESSION(304018@main): Triggers assertions in Site Isolation iOS API tests https://bugs.webkit.org/show_bug.cgi?id=305164 rdar://167455575
Reviewed by Alex Christensen.

304018@main added support for DeviceOrientationEvents in cross-site iframes with
Site Isolation enabled.

One of the fixes it made was to ensure that the cross-site iframe's WebProcess
could send IPC messages to the WebDeviceOrientationUpdateProviderProxy in the
UI Process. To make sure that WebDeviceOrientationUpdateProviderProxy was a
message receiver for this Web Process, new functions were added to it to add
and remove itself as a message receiver for the cross-site Web Process, which
would be called by the RemotePageProxy on its construction and destruction.

The crash report shows that WebDeviceOrientationUpdateProviderProxy is being
destroyed before the RemotePageProxy. So it is still registered as a message
receiver, causing this assertion to be hit: ASSERT(!m_messageReceiverMapCount);

So we refactor the code to follow a common pattern in site isolation:
WebDeviceOrientationUpdateProviderProxy will not receive messages directly from
the cross-site Web Processes. Rather, a new object is created which will receive
these messages and forward them to WebDeviceOrientationUpdateProviderProxy. This
new object will be the one registered as a message receiver and the RemotePageProxy
will register and unregister it from injectPageIntoNewProcess() and disconnect().
The new object is called RemotePageWebDeviceOrientationUpdateProviderProxy.

In order to forward messages to WebDeviceOrientationUpdateProviderProxy,
RemotePageWebDeviceOrientationUpdateProviderProxy must hold a reference to it.
This must be weak because it doesn't want to manage the lifetime of
WebDeviceOrientationUpdateProviderProxy. The issue here is that
WebDeviceOrientationUpdateProviderProxy inherits from two different classes that
both inherit from CanMakeWeakPtr so the construction of the WeakPtr is ambiguous
and fails. To fix this, we instead make RemotePageWebDeviceOrientationUpdateProviderProxy
hold a weak reference to one of these parent classes (MotionManagerClient) which
will dynamically downcasted to WebDeviceOrientationUpdateProviderProxy when needed.

The testing of the change is covered by the existing test:
(SiteIsolation.CrossSiteIFrameCanReceiveDeviceOrientationEvents) as well as the
tests that were broken by 304018@main and are fixed by this patch (such as

Source/WebCore/platform/ios/MotionManagerClient.h
@@ -41,6 +41,8 @@ public:
 
     virtual void orientationChanged(double, double, double, double, double) { }
     virtual void motionChanged(double, double, double, double, double, double, std::optional<double>, std::optional<double>, std::optional<double>) { }
+
+    virtual bool isWebDeviceOrientationUpdateProviderProxy() const { return false; }
 };
 
 };

Source/WebKit/UIProcess/RemotePageProxy.cpp
@@ -39,6 +39,7 @@
 #include "RemotePageFullscreenManagerProxy.h"
 #include "RemotePageScreenOrientationManagerProxy.h"
 #include "RemotePageVisitedLinkStoreRegistration.h"
+#include "RemotePageWebDeviceOrientationUpdateProviderProxy.h"
 #include "UserMediaProcessManager.h"
 #include "WebBackForwardList.h"
 #include "WebBackForwardListMessages.h"
@@ -89,20 +90,12 @@ RemotePageProxy::RemotePageProxy(WebPageProxy& page, WebProcessProxy& process, c
         m_messageReceiverRegistration.startReceivingMessages(m_process, m_webPageID, *this, page.backForwardList());
 
     m_process->addRemotePageProxy(*this);
-
-#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
-    m_page->webDeviceOrientationUpdateProviderProxy()->addAsMessageReceiverForProcess(m_process.get(), m_webPageID);
-#endif
 }
 
 void RemotePageProxy::disconnect()
 {
-    if (RefPtr page = m_page.get()) {
+    if (RefPtr page = m_page.get())
         page->isNoLongerAssociatedWithRemotePage(*this);
-#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
-        page->webDeviceOrientationUpdateProviderProxy()->removeAsMessageReceiverForProcess(m_process.get(), m_webPageID);
-#endif
-    }
     if (m_drawingArea)
         m_process->send(Messages::WebPage::Close(), m_webPageID);
     m_process->removeRemotePageProxy(*this);
@@ -113,6 +106,9 @@ void RemotePageProxy::disconnect()
 #endif
 #if ENABLE(VIDEO_PRESENTATION_MODE)
     m_videoPresentationManager = nullptr;
+#endif
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    m_webDeviceOrientationUpdateProvider = nullptr;
 #endif
 #if PLATFORM(IOS_FAMILY) || (PLATFORM(MAC) && ENABLE(VIDEO_PRESENTATION_MODE))
     m_playbackSessionManager = nullptr;
@@ -151,6 +147,9 @@ void RemotePageProxy::injectPageIntoNewProcess()
 #endif
 #if ENABLE(VIDEO_PRESENTATION_MODE)
     m_videoPresentationManager = RemotePageVideoPresentationManagerProxy::create(pageID(), m_process, page->protectedVideoPresentationManager().get());
+#endif
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    m_webDeviceOrientationUpdateProvider = RemotePageWebDeviceOrientationUpdateProviderProxy::create(pageID(), m_process, page->protectedWebDeviceOrientationUpdateProviderProxy().get());
 #endif
 #if PLATFORM(IOS_FAMILY) || (PLATFORM(MAC) && ENABLE(VIDEO_PRESENTATION_MODE))
     m_playbackSessionManager = RemotePagePlaybackSessionManagerProxy::create(pageID(), page->protectedPlaybackSessionManager().get(), m_process);

Source/WebKit/UIProcess/RemotePageProxy.h
@@ -74,6 +74,10 @@ class WebProcessActivityState;
 class WebProcessProxy;
 class WebScreenOrientationManagerProxy;
 
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+class RemotePageWebDeviceOrientationUpdateProviderProxy;
+#endif
+
 struct FrameInfoData;
 struct FrameTreeCreationParameters;
 struct NavigationActionData;
@@ -133,6 +137,9 @@ private:
 #endif
 #if ENABLE(VIDEO_PRESENTATION_MODE)
     RefPtr<RemotePageVideoPresentationManagerProxy> m_videoPresentationManager;
+#endif
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    RefPtr<RemotePageWebDeviceOrientationUpdateProviderProxy> m_webDeviceOrientationUpdateProvider;
 #endif
 #if PLATFORM(IOS_FAMILY) || (PLATFORM(MAC) && ENABLE(VIDEO_PRESENTATION_MODE))
     RefPtr<RemotePagePlaybackSessionManagerProxy> m_playbackSessionManager;

Source/WebKit/UIProcess/RemotePageWebDeviceOrientationUpdateProviderProxy.cpp
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2026 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "RemotePageWebDeviceOrientationUpdateProviderProxy.h"
+
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+
+#include "WebDeviceOrientationUpdateProviderProxy.h"
+#include "WebDeviceOrientationUpdateProviderProxyMessages.h"
+#include "WebPageProxy.h"
+#include "WebProcessProxy.h"
+#include <WebCore/MotionManagerClient.h>
+
+namespace WebKit {
+
+Ref<RemotePageWebDeviceOrientationUpdateProviderProxy> RemotePageWebDeviceOrientationUpdateProviderProxy::create(WebCore::PageIdentifier pageID, WebProcessProxy& process, WebCore::MotionManagerClient* client)
+{
+    return adoptRef(*new RemotePageWebDeviceOrientationUpdateProviderProxy(pageID, process, client));
+}
+
+RemotePageWebDeviceOrientationUpdateProviderProxy::RemotePageWebDeviceOrientationUpdateProviderProxy(WebCore::PageIdentifier pageID, WebProcessProxy& process, WebCore::MotionManagerClient* client)
+    : m_identifier(pageID)
+    , m_client(client)
+    , m_process(process)
+{
+    process.addMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), m_identifier, *this);
+}
+
+RemotePageWebDeviceOrientationUpdateProviderProxy::~RemotePageWebDeviceOrientationUpdateProviderProxy()
+{
+    m_process->removeMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), m_identifier);
+}
+
+void RemotePageWebDeviceOrientationUpdateProviderProxy::didReceiveMessage(IPC::Connection& connection, IPC::Decoder& decoder)
+{
+    if (RefPtr provider = dynamicDowncast<WebDeviceOrientationUpdateProviderProxy>(m_client.get()))
+        provider->didReceiveMessage(connection, decoder);
+}
+
+}
+#endif // PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)

Source/WebKit/UIProcess/RemotePageWebDeviceOrientationUpdateProviderProxy.h
@@ -1,0 +1,62 @@
+/*
+ * Copyright (C) 2026 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+
+#include "MessageReceiver.h"
+#include <WebCore/PageIdentifier.h>
+
+namespace WebCore {
+class MotionManagerClient;
+}
+
+namespace WebKit {
+
+class WebProcessProxy;
+
+class RemotePageWebDeviceOrientationUpdateProviderProxy : public IPC::MessageReceiver, public RefCounted<RemotePageWebDeviceOrientationUpdateProviderProxy> {
+public:
+    static Ref<RemotePageWebDeviceOrientationUpdateProviderProxy> create(WebCore::PageIdentifier, WebProcessProxy&, WebCore::MotionManagerClient*);
+
+    ~RemotePageWebDeviceOrientationUpdateProviderProxy();
+
+    void ref() const final { RefCounted::ref(); }
+    void deref() const final { RefCounted::deref(); }
+
+private:
+    RemotePageWebDeviceOrientationUpdateProviderProxy(WebCore::PageIdentifier, WebProcessProxy&, WebCore::MotionManagerClient*);
+
+    void didReceiveMessage(IPC::Connection&, IPC::Decoder&) final;
+
+    const WebCore::PageIdentifier m_identifier;
+    const WeakPtr<WebCore::MotionManagerClient> m_client;
+    const Ref<WebProcessProxy> m_process;
+};
+
+}
+
+#endif // PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -10363,6 +10363,13 @@ void WebPageProxy::setMockVideoPresentationModeEnabled(bool enabled)
 
 #endif
 
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+RefPtr<WebDeviceOrientationUpdateProviderProxy> WebPageProxy::protectedWebDeviceOrientationUpdateProviderProxy()
+{
+    return m_webDeviceOrientationUpdateProviderProxy;
+}
+#endif
+
 #if ENABLE(VIDEO) || ENABLE(WEB_AUDIO)
 void WebPageProxy::addRemoteMediaSessionManager(WebCore::PageIdentifier localPageIdentifier)
 {

Source/WebKit/UIProcess/WebPageProxy.h
@@ -835,6 +835,10 @@ public:
     void setMockVideoPresentationModeEnabled(bool);
 #endif
 
+#if PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)
+    RefPtr<WebDeviceOrientationUpdateProviderProxy> protectedWebDeviceOrientationUpdateProviderProxy();
+#endif
+
     WebScreenOrientationManagerProxy* screenOrientationManager() { return m_screenOrientationManager.get(); }
 
 #if ENABLE(VIDEO) || ENABLE(WEB_AUDIO)

Source/WebKit/UIProcess/ios/WebDeviceOrientationUpdateProviderProxy.h
@@ -67,10 +67,9 @@ public:
     void decrementCheckedPtrCount() const final { CanMakeCheckedPtr::decrementCheckedPtrCount(); }
     void setDidBeginCheckedPtrDeletion() final { CanMakeCheckedPtr::setDidBeginCheckedPtrDeletion(); }
 
-    void addAsMessageReceiverForProcess(WebProcessProxy&, WebCore::PageIdentifier);
-    void removeAsMessageReceiverForProcess(WebProcessProxy&, WebCore::PageIdentifier);
-
 private:
+    friend class RemotePageWebDeviceOrientationUpdateProviderProxy;
+
     explicit WebDeviceOrientationUpdateProviderProxy(WebPageProxy&);
 
     // WebCore::WebCoreMotionManagerClient
@@ -80,9 +79,15 @@ private:
     // IPC::MessageReceiver
     void didReceiveMessage(IPC::Connection&, IPC::Decoder&) override;
 
+    bool isWebDeviceOrientationUpdateProviderProxy() const final { return true; }
+
     WeakPtr<WebPageProxy> m_page;
 };
 
 } // namespace WebKit
+
+SPECIALIZE_TYPE_TRAITS_BEGIN(WebKit::WebDeviceOrientationUpdateProviderProxy)
+    static bool isType(const WebCore::MotionManagerClient& client) { return client.isWebDeviceOrientationUpdateProviderProxy(); }
+SPECIALIZE_TYPE_TRAITS_END()
 
 #endif // PLATFORM(IOS_FAMILY) && ENABLE(DEVICE_ORIENTATION)

Source/WebKit/UIProcess/ios/WebDeviceOrientationUpdateProviderProxy.mm
@@ -57,16 +57,6 @@ WebDeviceOrientationUpdateProviderProxy::~WebDeviceOrientationUpdateProviderProx
         page->protectedLegacyMainFrameProcess()->removeMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), page->webPageIDInMainFrameProcess());
 }
 
-void WebDeviceOrientationUpdateProviderProxy::addAsMessageReceiverForProcess(WebProcessProxy& process, WebCore::PageIdentifier pageID)
-{
-    process.addMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), pageID, *this);
-}
-
-void WebDeviceOrientationUpdateProviderProxy::removeAsMessageReceiverForProcess(WebProcessProxy& process, WebCore::PageIdentifier pageID)
-{
-    process.removeMessageReceiver(Messages::WebDeviceOrientationUpdateProviderProxy::messageReceiverName(), pageID);
-}
-
 void WebDeviceOrientationUpdateProviderProxy::startUpdatingDeviceOrientation()
 {
     [[WebCoreMotionManager sharedManager] addOrientationClient:this];

Source/WebKit/WebKit.xcodeproj/project.pbxproj
@@ -6923,6 +6923,8 @@
 		7BDD9DDA28D205C6004CDF48 /* MessageObserver.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = MessageObserver.h; sourceTree = "<group>"; };
 		7BDD9DDB28D205C6004CDF48 /* WorkQueueMessageReceiver.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = WorkQueueMessageReceiver.h; sourceTree = "<group>"; };
 		7BDDA33F275652EA0038659E /* AuxiliaryProcessCreationParameters.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = AuxiliaryProcessCreationParameters.h; sourceTree = "<group>"; };
+		7BDE98AC2F0F33140091DCA3 /* RemotePageWebDeviceOrientationUpdateProviderProxy.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = RemotePageWebDeviceOrientationUpdateProviderProxy.h; sourceTree = "<group>"; };
+		7BDE98AD2F0F33B20091DCA3 /* RemotePageWebDeviceOrientationUpdateProviderProxy.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = RemotePageWebDeviceOrientationUpdateProviderProxy.cpp; sourceTree = "<group>"; };
 		7BE005B52E5C4E050014F7CF /* RemoteDisplayListRecorderProxy.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = RemoteDisplayListRecorderProxy.h; sourceTree = "<group>"; };
 		7BE005B62E5C4E050014F7CF /* RemoteDisplayListRecorderProxy.cpp */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.cpp; path = RemoteDisplayListRecorderProxy.cpp; sourceTree = "<group>"; };
 		7BE005B72E5C4E390014F7CF /* RemoteDisplayListRecorder.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = RemoteDisplayListRecorder.h; sourceTree = "<group>"; };
@@ -15408,6 +15410,8 @@
 				FA1A7DE52E012DB3005E394B /* RemotePageVideoPresentationManagerProxy.cpp */,
 				FA1A7DE42E012DB3005E394B /* RemotePageVideoPresentationManagerProxy.h */,
 				5CEB40A22A534EE100563C91 /* RemotePageVisitedLinkStoreRegistration.h */,
+				7BDE98AD2F0F33B20091DCA3 /* RemotePageWebDeviceOrientationUpdateProviderProxy.cpp */,
+				7BDE98AC2F0F33140091DCA3 /* RemotePageWebDeviceOrientationUpdateProviderProxy.h */,
 				BC111B08112F5E3C00337BAB /* ResponsivenessTimer.cpp */,
 				1A30066C1110F4F70031937C /* ResponsivenessTimer.h */,
 				93D6B7B0255268A10058DD3A /* SpeechRecognitionPermissionManager.cpp */,