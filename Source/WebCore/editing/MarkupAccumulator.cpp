szewai/WebKit

commit b3e11f0a00aa4620e1a3c7c26402913280b6bdea

Escape < and > when serializing HTML attribute values https://bugs.webkit.org/show_bug.cgi?id=292432 rdar://150520333
Reviewed by Ryosuke Niwa and Sam Weinig.

Implement https://github.com/whatwg/html/pull/6362 and add a preference
in case we find out we have to disable it in certain cases.

The preference does not impact the recently added getHTML() method on
Element and ShadowRoot as that should be new enough to not matter.

Add new test from: https://github.com/web-platform-tests/wpt/pull/27501

Canonical link: https://commits.webkit.org/295149@main

Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml
@@ -3229,6 +3229,20 @@ GoogleAntiFlickerOptimizationQuirkEnabled:
     WebCore:
       default: true
 
+HTMLLegacyAttributeValueSerializationEnabled:
+   type: bool
+   status: internal
+   category: dom
+   humanReadableName: "HTML legacy attribute value serialization"
+   humanReadableDescription: "Enable HTML legacy attribute value serialization"
+   defaultValue:
+     WebKitLegacy:
+       default: false
+     WebKit:
+       default: false
+     WebCore:
+       default: false
+
 HTTPEquivEnabled:
   type: bool
   status: mature

Source/WebCore/editing/MarkupAccumulator.cpp
@@ -445,8 +445,20 @@ StringBuilder MarkupAccumulator::takeMarkup()
 
 void MarkupAccumulator::appendAttributeValue(StringBuilder& result, const String& attribute)
 {
-    appendCharactersReplacingEntities(result, attribute,
-        inXMLFragmentSerialization() ? EntityMaskInAttributeValue : EntityMaskInHTMLAttributeValue);
+    auto entityMask = [&] -> OptionSet<EntityMask> {
+        switch (m_serializationSyntax) {
+        case SerializationSyntax::XML:
+            return EntityMaskInAttributeValue;
+        case SerializationSyntax::HTML:
+            return EntityMaskInHTMLAttributeValue;
+        case SerializationSyntax::HTMLLegacyAttributeValue:
+            return EntityMaskInHTMLLegacyAttributeValue;
+        default:
+            ASSERT_NOT_REACHED();
+            return EntityMaskInAttributeValue;
+        }
+    }();
+    appendCharactersReplacingEntities(result, attribute, entityMask);
 }
 
 void MarkupAccumulator::appendCustomAttributes(StringBuilder&, const Element&, Namespaces*)
@@ -885,6 +897,13 @@ static bool isElementExcludedByRule(const MarkupExclusionRule& rule, const Eleme
 bool MarkupAccumulator::shouldExcludeElement(const Element& element)
 {
     return std::ranges::any_of(m_exclusionRules, std::bind(isElementExcludedByRule, std::placeholders::_1, std::ref(element)));
+}
+
+SerializationSyntax MarkupAccumulator::serializationSyntax(Document& document)
+{
+    if (!document.isHTMLDocument())
+        return SerializationSyntax::XML;
+    return document.settings().htmlLegacyAttributeValueSerializationEnabled() ? SerializationSyntax::HTMLLegacyAttributeValue : SerializationSyntax::HTML;
 }
 
 }

Source/WebCore/editing/MarkupAccumulator.h
@@ -63,7 +63,8 @@ constexpr OptionSet<EntityMask> EntityMaskInPCDATA = { EntityMask::Amp, EntityMa
 constexpr auto EntityMaskInHTMLPCDATA = EntityMaskInPCDATA | EntityMask::Nbsp;
 constexpr OptionSet<EntityMask> EntityMaskInAttributeValue = { EntityMask::Amp, EntityMask::Lt, EntityMask::Gt,
     EntityMask::Quot, EntityMask::Tab, EntityMask::LineFeed, EntityMask::CarriageReturn };
-constexpr auto EntityMaskInHTMLAttributeValue = { EntityMask::Amp, EntityMask::Quot, EntityMask::Nbsp };
+constexpr auto EntityMaskInHTMLAttributeValue = { EntityMask::Amp, EntityMask::Lt, EntityMask::Gt, EntityMask::Quot, EntityMask::Nbsp };
+constexpr auto EntityMaskInHTMLLegacyAttributeValue = { EntityMask::Amp, EntityMask::Quot, EntityMask::Nbsp };
 
 class MarkupAccumulator {
     WTF_MAKE_NONCOPYABLE(MarkupAccumulator);
@@ -75,6 +76,8 @@ public:
 
     static void appendCharactersReplacingEntities(StringBuilder&, const String&, OptionSet<EntityMask>);
     void enableURLReplacement(UncheckedKeyHashMap<String, String>&& replacementURLStrings, UncheckedKeyHashMap<Ref<CSSStyleSheet>, String>&& replacementURLStringsForCSSStyleSheet);
+
+    static SerializationSyntax serializationSyntax(Document&);
 
 protected:
     unsigned length() const { return m_markup.length(); }

Source/WebCore/editing/markup.h
@@ -104,7 +104,7 @@ String serializePreservingVisualAppearance(const VisibleSelection&, ResolveURLs 
     IgnoreUserSelectNone = IgnoreUserSelectNone::Yes, PreserveBaseElement = PreserveBaseElement::No, PreserveDirectionForInlineText = PreserveDirectionForInlineText::No, Vector<Ref<Node>>* = nullptr);
 
 enum class SerializedNodes : uint8_t { SubtreeIncludingNode, SubtreesOfChildren };
-enum class SerializationSyntax : uint8_t { HTML, XML };
+enum class SerializationSyntax : uint8_t { HTML, XML, HTMLLegacyAttributeValue };
 enum class SerializeShadowRoots : uint8_t { Explicit, Serializable, AllForInterchange };
 WEBCORE_EXPORT String serializeFragment(const Node&, SerializedNodes, Vector<Ref<Node>>* = nullptr, ResolveURLs = ResolveURLs::No, std::optional<SerializationSyntax> = std::nullopt, SerializeShadowRoots = SerializeShadowRoots::Explicit, Vector<Ref<ShadowRoot>>&& explicitShadowRoots = { }, const Vector<MarkupExclusionRule>& exclusionRules = { });
 WEBCORE_EXPORT String serializeFragmentWithURLReplacement(const Node&, SerializedNodes, Vector<Ref<Node>>*, ResolveURLs, std::optional<SerializationSyntax>, UncheckedKeyHashMap<String, String>&& replacementURLStrings, UncheckedKeyHashMap<Ref<CSSStyleSheet>, String>&& replacementURLStringsForCSSStyleSheet, SerializeShadowRoots = SerializeShadowRoots::Explicit, Vector<Ref<ShadowRoot>>&& explicitShadowRoots = { }, const Vector<MarkupExclusionRule>& exclusionRules = { });

LayoutTests/editing/pasteboard/paste-noscript-expected.txt
@@ -15,7 +15,7 @@ undefined
 undefined
 <iframe id="iframe1" src="javascript:var x = 1;" style="width: 200px; height: 100px; background-color:#cee;"></iframe>
 undefined
-<iframe id="iframe2" srcdoc="<script>var x = 1;</script>" style="width: 200px; height: 100px; background-color:#cee;"></iframe>
+<iframe id="iframe2" srcdoc="&lt;script&gt;var x = 1;&lt;/script&gt;" style="width: 200px; height: 100px; background-color:#cee;"></iframe>
 <form id="form1" formaction="javascript:sayHello()" style="width: 200px; height: 150px; background-color: rgb(204, 238, 238);">This is a form<br><img src="../resources/abe.png"><button>Submit.</button></form>
 <form id="form1" action="javascript:sayHello()" formaction="javascript:sayHello()" style="width: 200px; height: 150px; background-color:#cee;">This is a form<br><img src="../resources/abe.png"><button formaction="javascript:sayHello()">Submit.</button></form>
 undefined

LayoutTests/imported/w3c/web-platform-tests/css/css-flexbox/flex-aspect-ratio-img-column-017-expected.txt
@@ -1,6 +1,6 @@
 
 FAIL img 1 assert_equals:
-<img src="data:image/svg+xml,<svg xmlns=&quot;http://www.w3.org/2000/svg&quot; />" style="height: 200px; background: green;" data-expected-height="150">
+<img src="data:image/svg+xml,&lt;svg xmlns=&quot;http://www.w3.org/2000/svg&quot; /&gt;" style="height: 200px; background: green;" data-expected-height="150">
 height expected 150 but got 0
 Specified size suggestion = 200px
 Content size suggestion = 150px [1]

LayoutTests/imported/w3c/web-platform-tests/html/semantics/embedded-content/the-img-element/current-pixel-density/basic-expected.txt
@@ -10,7 +10,7 @@ PASS <img srcset="/images/green-256x256.png 512w" sizes="256px" data-expect="128
 PASS <img srcset="/images/green-256x256.png 256w" sizes="512px" data-expect="512">
 PASS <img srcset="/images/green-256x256.png 256w" sizes="1px" data-expect="1">
 PASS <img srcset="/images/green-256x256.png 256w" sizes="0px" data-expect="0">
-PASS <img srcset="data:image/svg+xml,<svg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='-1%20-1%202%202'%20width='20'%20height='20'><circle%20r='1'/></svg> 2x" data-expect="10">
-PASS <img srcset="data:image/svg+xml,<svg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='-1%20-1%202%202'%20width='20'><circle%20r='1'/></svg> 2x" data-expect="10">
-PASS <img srcset="data:image/svg+xml,<svg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='-1%20-1%202%202'%20height='20'><circle%20r='1'/></svg> 2x" data-expect="10">
+PASS <img srcset="data:image/svg+xml,&lt;svg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='-1%20-1%202%202'%20width='20'%20height='20'&gt;&lt;circle%20r='1'/&gt;&lt;/svg&gt; 2x" data-expect="10">
+PASS <img srcset="data:image/svg+xml,&lt;svg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='-1%20-1%202%202'%20width='20'&gt;&lt;circle%20r='1'/&gt;&lt;/svg&gt; 2x" data-expect="10">
+PASS <img srcset="data:image/svg+xml,&lt;svg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='-1%20-1%202%202'%20height='20'&gt;&lt;circle%20r='1'/&gt;&lt;/svg&gt; 2x" data-expect="10">
 

LayoutTests/imported/w3c/web-platform-tests/html/syntax/serializing-html-fragments/serializing-expected.txt
@@ -6,9 +6,9 @@ PASS innerHTML 3 <a b="c"></a>
 PASS innerHTML 4 <a b="&amp;"></a>
 PASS innerHTML 5 <a b="&nbsp;"></a>
 PASS innerHTML 6 <a b="&quot;"></a>
-PASS innerHTML 7 <a b="<"></a>
-PASS innerHTML 8 <a b=">"></a>
-PASS innerHTML 9 <a href="javascript:&quot;<>&quot;"></a>
+PASS innerHTML 7 <a b="&lt;"></a>
+PASS innerHTML 8 <a b="&gt;"></a>
+PASS innerHTML 9 <a href="javascript:&quot;&lt;&gt;&quot;"></a>
 PASS innerHTML 10 <svg xlink:href="a"></svg>
 PASS innerHTML 11 <svg xmlns:svg="test"></svg>
 PASS innerHTML 12 a
@@ -35,9 +35,9 @@ PASS outerHTML 3 <span><a b="c"></a></span>
 PASS outerHTML 4 <span><a b="&amp;"></a></span>
 PASS outerHTML 5 <span><a b="&nbsp;"></a></span>
 PASS outerHTML 6 <span><a b="&quot;"></a></span>
-PASS outerHTML 7 <span><a b="<"></a></span>
-PASS outerHTML 8 <span><a b=">"></a></span>
-PASS outerHTML 9 <span><a href="javascript:&quot;<>&quot;"></a></span>
+PASS outerHTML 7 <span><a b="&lt;"></a></span>
+PASS outerHTML 8 <span><a b="&gt;"></a></span>
+PASS outerHTML 9 <span><a href="javascript:&quot;&lt;&gt;&quot;"></a></span>
 PASS outerHTML 10 <span><svg xlink:href="a"></svg></span>
 PASS outerHTML 11 <span><svg xmlns:svg="test"></svg></span>
 PASS outerHTML 12 <span>a</span>

commit 3328a2303885115f5e6be4a0534077446d883e6f

Remove HTMLLegacyAttributeValueSerializationEnabled preference https://bugs.webkit.org/show_bug.cgi?id=305371
Reviewed by Tim Nguyen.

Remove obsolete code as there was no fallout from 295149@main. Also
improve the switch statement slightly so new unhandled enum values
result in a compilation error.

Canonical link: https://commits.webkit.org/305515@main

Source/WTF/Scripts/Preferences/UnifiedWebPreferences.yaml
@@ -3562,20 +3562,6 @@ HTMLEnhancedSelectSelectedContentEnabled:
      WebCore:
        default: false
 
-HTMLLegacyAttributeValueSerializationEnabled:
-   type: bool
-   status: internal
-   category: dom
-   humanReadableName: "HTML legacy attribute value serialization"
-   humanReadableDescription: "Enable HTML legacy attribute value serialization"
-   defaultValue:
-     WebKitLegacy:
-       default: false
-     WebKit:
-       default: false
-     WebCore:
-       default: false
-
 HTTPEquivEnabled:
   type: bool
   status: mature

Source/WebCore/editing/MarkupAccumulator.cpp
@@ -466,12 +466,9 @@ void MarkupAccumulator::appendAttributeValue(StringBuilder& result, const String
             return EntityMaskInAttributeValue;
         case SerializationSyntax::HTML:
             return EntityMaskInHTMLAttributeValue;
-        case SerializationSyntax::HTMLLegacyAttributeValue:
-            return EntityMaskInHTMLLegacyAttributeValue;
-        default:
-            ASSERT_NOT_REACHED();
-            return EntityMaskInAttributeValue;
         }
+        ASSERT_NOT_REACHED();
+        return EntityMaskInAttributeValue;
     }();
     appendCharactersReplacingEntities(result, attribute, entityMask);
 }
@@ -916,9 +913,7 @@ bool MarkupAccumulator::shouldExcludeElement(const Element& element)
 
 SerializationSyntax MarkupAccumulator::serializationSyntax(Document& document)
 {
-    if (!document.isHTMLDocument())
-        return SerializationSyntax::XML;
-    return document.settings().htmlLegacyAttributeValueSerializationEnabled() ? SerializationSyntax::HTMLLegacyAttributeValue : SerializationSyntax::HTML;
+    return document.isHTMLDocument() ? SerializationSyntax::HTML : SerializationSyntax::XML;
 }
 
 }

Source/WebCore/editing/MarkupAccumulator.h
@@ -64,7 +64,6 @@ constexpr auto EntityMaskInHTMLPCDATA = EntityMaskInPCDATA | EntityMask::Nbsp;
 constexpr OptionSet<EntityMask> EntityMaskInAttributeValue = { EntityMask::Amp, EntityMask::Lt, EntityMask::Gt,
     EntityMask::Quot, EntityMask::Tab, EntityMask::LineFeed, EntityMask::CarriageReturn };
 constexpr auto EntityMaskInHTMLAttributeValue = { EntityMask::Amp, EntityMask::Lt, EntityMask::Gt, EntityMask::Quot, EntityMask::Nbsp };
-constexpr auto EntityMaskInHTMLLegacyAttributeValue = { EntityMask::Amp, EntityMask::Quot, EntityMask::Nbsp };
 
 class MarkupAccumulator {
     WTF_MAKE_NONCOPYABLE(MarkupAccumulator);

Source/WebCore/editing/markup.h
@@ -104,7 +104,7 @@ String serializePreservingVisualAppearance(const VisibleSelection&, ResolveURLs 
     IgnoreUserSelectNone = IgnoreUserSelectNone::Yes, PreserveBaseElement = PreserveBaseElement::No, PreserveDirectionForInlineText = PreserveDirectionForInlineText::No, Vector<Ref<Node>>* = nullptr);
 
 enum class SerializedNodes : uint8_t { SubtreeIncludingNode, SubtreesOfChildren };
-enum class SerializationSyntax : uint8_t { HTML, XML, HTMLLegacyAttributeValue };
+enum class SerializationSyntax : bool { HTML, XML };
 enum class SerializeShadowRoots : uint8_t { Explicit, Serializable, AllForInterchange };
 WEBCORE_EXPORT String serializeFragment(const Node&, SerializedNodes, Vector<Ref<Node>>* = nullptr, ResolveURLs = ResolveURLs::No, std::optional<SerializationSyntax> = std::nullopt, SerializeShadowRoots = SerializeShadowRoots::Explicit, Vector<Ref<ShadowRoot>>&& explicitShadowRoots = { }, const Vector<MarkupExclusionRule>& exclusionRules = { });
 WEBCORE_EXPORT String serializeFragmentWithURLReplacement(const Node&, SerializedNodes, Vector<Ref<Node>>*, ResolveURLs, std::optional<SerializationSyntax>, HashMap<String, String>&& replacementURLStrings, HashMap<Ref<CSSStyleSheet>, String>&& replacementURLStringsForCSSStyleSheet, SerializeShadowRoots = SerializeShadowRoots::Explicit, Vector<Ref<ShadowRoot>>&& explicitShadowRoots = { }, const Vector<MarkupExclusionRule>& exclusionRules = { });