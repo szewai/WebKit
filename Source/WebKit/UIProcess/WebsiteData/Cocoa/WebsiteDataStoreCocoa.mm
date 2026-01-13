baabf98cc21cfd90a2a827380de054b0f521548c
Nakul Bajaj <nakul_bajaj@apple.com>
2026-01-13 10:04:36 -0800

    Allow using new network loader for WebKit generally (non-Safari) rdar://166146977 https://bugs.webkit.org/show_bug.cgi?id=303843
    Reviewed by Alex Christensen.
    
    * Source/WebCore/PAL/pal/spi/cocoa/NetworkSPI.h:
    * Source/WebKit/Configurations/AllowedSPI-legacy.toml:
    * Source/WebKit/UIProcess/WebsiteData/Cocoa/WebsiteDataStoreCocoa.mm:
    (WebKit::WebsiteDataStore::useNetworkLoader):
    
    Canonical link: https://commits.webkit.org/305519@main

--- Source/WebKit/UIProcess/WebsiteData/Cocoa/WebsiteDataStoreCocoa.mm
+++ Source/WebKit/UIProcess/WebsiteData/Cocoa/WebsiteDataStoreCocoa.mm
@@ -255,10 +255,14 @@ std::optional<bool> WebsiteDataStore::useNetworkLoader()
 #if HAVE(NWSETTINGS_UNIFIED_HTTP) && defined(NW_SETTINGS_HAS_UNIFIED_HTTP)
     if (isRunningTest(applicationBundleIdentifier()))
         return true;
-    if (nw_settings_get_unified_http_enabled())
-        return isSafari;
-#endif
+    if (nw_settings_get_unified_http_enabled() && isSafari)
+        return true;
+#endif // HAVE(NWSETTINGS_UNIFIED_HTTP) && defined(NW_SETTINGS_HAS_UNIFIED_HTTP)
+#if HAVE(NWSETTINGS_UNIFIED_HTTP_WEBKIT)
+    return nw_settings_get_unified_http_enabled_webkit();
+#else
     return std::nullopt;
+#endif
 
 #endif // NETWORK_LOADER
 }
