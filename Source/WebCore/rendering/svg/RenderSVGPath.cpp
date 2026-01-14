19059dff60c1e3c6a55bf861f8bba78cfc28ce22
Rob Buis <rbuis@igalia.com>
2026-01-14 08:23:32 -0800

    [LBSE] Fix path-seg-nan-coordinates-linecap-crash.html crash https://bugs.webkit.org/show_bug.cgi?id=305471
    Reviewed by Nikolas Zimmermann.
    
    In 299291@main path-seg-nan-coordinates-linecap-crash.html was added to test
    inserting a PathSeg with NaN coordinates, but the fix was only implemented for
    Legacy SVG. This PR adds the same fix for LBSE.
    
    * Source/WebCore/rendering/svg/RenderSVGPath.cpp:
    (WebCore::RenderSVGPath::adjustStrokeBoundingBoxForZeroLengthLinecaps const):
    
    Canonical link: https://commits.webkit.org/305577@main

--- Source/WebCore/rendering/svg/RenderSVGPath.cpp
+++ Source/WebCore/rendering/svg/RenderSVGPath.cpp
@@ -78,8 +78,11 @@ FloatRect RenderSVGPath::adjustStrokeBoundingBoxForZeroLengthLinecaps(RepaintRec
     if (style().hasStroke()) {
         // FIXME: zero-length subpaths do not respect vector-effect = non-scaling-stroke.
         float strokeWidth = this->strokeWidth();
-        for (size_t i = 0; i < m_zeroLengthLinecapLocations.size(); ++i)
-            strokeBoundingBox.unite(zeroLengthSubpathRect(m_zeroLengthLinecapLocations[i], strokeWidth));
+        for (auto& zeroLengthLinecapLocation : m_zeroLengthLinecapLocations) {
+            auto subpathRect = zeroLengthSubpathRect(zeroLengthLinecapLocation, strokeWidth);
+            if (!subpathRect.isNaN())
+                strokeBoundingBox.unite(subpathRect);
+        }
     }
 
     return strokeBoundingBox;
